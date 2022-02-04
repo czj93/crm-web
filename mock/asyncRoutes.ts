ff60865fbe0922047e119c0bcdecd0f2c70bcf2f
caozhijian <zhijian.cao@geely.com>
2022-02-04 15:42:51 +0800

    refactor(view): 删除权限相关代码

--- mock/asyncRoutes.ts
+++ mock/asyncRoutes.ts
@@ -1,45 +1,45 @@
 // 根据角色动态生成路由
 import { MockMethod } from "vite-plugin-mock";
 
-const permissionRouter = {
-  path: "/permission",
-  name: "permission",
-  redirect: "/permission/page/index",
-  meta: {
-    title: "menus.permission",
-    icon: "lollipop",
-    i18n: true,
-    showLink: false,
-    rank: 3
-  },
-  children: [
-    {
-      path: "/permission/page/index",
-      name: "permissionPage",
-      meta: {
-        title: "menus.permissionPage",
-        i18n: true,
-        showLink: true
-      }
-    },
-    {
-      path: "/permission/button/index",
-      name: "permissionButton",
-      meta: {
-        title: "menus.permissionButton",
-        i18n: true,
-        showLink: true,
-        authority: []
-      }
-    }
-  ]
-};
+// const permissionRouter = {
+//   path: "/permission",
+//   name: "permission",
+//   redirect: "/permission/page/index",
+//   meta: {
+//     title: "menus.permission",
+//     icon: "lollipop",
+//     i18n: true,
+//     showLink: false,
+//     rank: 3
+//   },
+//   children: [
+//     {
+//       path: "/permission/page/index",
+//       name: "permissionPage",
+//       meta: {
+//         title: "menus.permissionPage",
+//         i18n: true,
+//         showLink: true
+//       }
+//     },
+//     {
+//       path: "/permission/button/index",
+//       name: "permissionButton",
+//       meta: {
+//         title: "menus.permissionButton",
+//         i18n: true,
+//         showLink: true,
+//         authority: []
+//       }
+//     }
+//   ]
+// };
 
 // 添加不同按钮权限到/permission/button页面中
-function setDifAuthority(authority, routes) {
-  routes.children[1].meta.authority = [authority];
-  return routes;
-}
+// function setDifAuthority(authority, routes) {
+//   routes.children[1].meta.authority = [authority];
+//   return routes;
+// }
 
 export default [
   {
@@ -49,12 +49,14 @@ export default [
       if (query.name === "admin") {
         return {
           code: 0,
-          info: [setDifAuthority("v-admin", permissionRouter)]
+          info: []
+          // info: [setDifAuthority("v-admin", permissionRouter)]
         };
       } else {
         return {
           code: 0,
-          info: [setDifAuthority("v-test", permissionRouter)]
+          info: []
+          // info: [setDifAuthority("v-test", permissionRouter)]
         };
       }
     }
